0f8c1f9b9f2e0a9bc081de3895e02bac057f82b1
CristianSandu <alin-cristinel.sandu@univr.it>
2018-04-09 09:01:18 -0700

    aggiornato il launch file

---
+++ launch/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Avvio nodo roscore
+roscore &
+# Metto in sleep per attendere che parta il nodo roscore
+sleep 1
+# Faccio partire il primo nodo e li 
+gnome-terminal -x sh -c "rosrun ROS_Homework_1 publisher"
+# Faccio partire il terzo nodo
+gnome-terminal -x sh -c "rosrun ROS_Homework_1 viewer"
+# Faccio partire il secondo nodo
+gnome-terminal -x sh -c "rosrun ROS_Homework_1 selector"
+
